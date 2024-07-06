be5070cdfb193716baf3dab02281f29f8e100e64
Marius Fagerland <mariusfagerland@yahoo.no>
2024-07-06 17:13:50 +0200

    Add userid and requestid to tracelog

--- logging/tracelog/tracelog.go
+++ logging/tracelog/tracelog.go
@@ -12,12 +12,12 @@ func SetAppName(appName string) {
 	tracelogger.appName = appName
 }
 
-func Info(payload string) {
-	tracelogger.Info(payload)
+func Info(payload string, userId string, requestId string) {
+	tracelogger.Info(payload, userId, requestId)
 }
 
-func Error(payload string) {
-	tracelogger.Error(payload)
+func Error(payload string, userId string, requestId string) {
+	tracelogger.Error(payload, userId, requestId)
 }
 
 type TraceLogger struct {
@@ -28,11 +28,11 @@ func NewTraceLogger(appName string) *TraceLogger {
 	return &TraceLogger{appName: appName}
 }
 
-func (tl *TraceLogger) Info(payload string) {
+func (tl *TraceLogger) Info(payload string, userId string, requestId string) {
 	logLevel := "INFO"
 	logType := "TRACE"
 
-	entry := newTraceLog(logLevel, logType, payload, tl.appName)
+	entry := newTraceLog(logLevel, logType, payload, tl.appName, userId, requestId)
 	jsonEntry, err := json.Marshal(entry)
 	if err != nil {
 		log.Println(err.Error())
@@ -40,11 +40,11 @@ func (tl *TraceLogger) Info(payload string) {
 	log.Println(string(jsonEntry))
 }
 
-func (tl *TraceLogger) Error(payload string) {
+func (tl *TraceLogger) Error(payload string, userId string, requestId string) {
 	logLevel := "ERROR"
 	logType := "TRACE"
 
-	entry := newTraceLog(logLevel, logType, payload, tl.appName)
+	entry := newTraceLog(logLevel, logType, payload, tl.appName, userId, requestId)
 	jsonEntry, err := json.Marshal(entry)
 	if err != nil {
 		log.Println(err.Error())
@@ -58,9 +58,11 @@ type traceLog struct {
 	LogType   string `json:"log_type"`
 	AppName   string `json:"app_name"`
 	Payload   string `json:"payload"`
+	UserId    string `json:"user_id"`
+	RequestId string `json:"request_id"`
 }
 
-func newTraceLog(logLevel string, logType string, payload string, appName string) *traceLog {
+func newTraceLog(logLevel string, logType string, payload string, appName string, userId string, requestId string) *traceLog {
 	currentTime := time.Now()
 	return &traceLog{
 		Timestamp: currentTime.Format("2006-01-02T15:04:05.000-07:00"),
@@ -68,5 +70,7 @@ func newTraceLog(logLevel string, logType string, payload string, appName string
 		LogType:   logType,
 		Payload:   payload,
 		AppName:   appName,
+		UserId:    userId,
+		RequestId: requestId,
 	}
 }
