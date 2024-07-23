109081a8bd097afa441b5a064b32745e4771407a
Marius Fagerland <mariusfagerland@yahoo.no>
2024-07-23 20:03:35 +0200

    Impl test for happy case user auth

--- middleware/auth_test.go
+++ middleware/auth_test.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/mariusfa/golf/auth"
 )
 
 func helloAuthHandler() http.Handler {
@@ -34,7 +36,39 @@ func (fl *fakeLogger) Info(message string, requestId string) { fl.InfoMessage = 
 func (fl *fakeLogger) Error(message string, requestId string) { fl.ErrorMessage = message }
 
 func TestFindUser(t *testing.T) {
-	t.Errorf("Test not implemented")
+	fakeLogger := &fakeLogger{}
+	fakeUesrRepo := &fakeUserRepository{
+		UserList: map[string]*User{
+			"123": {Id: "123", Name: "John"},
+		},
+	}
+	token, _ := auth.CreateToken("123", "secret", nil)
+	headerValue := "Bearer " + token
+
+	authParams := NewAuthParams("secret", fakeUesrRepo, fakeLogger)
+	handler := helloAuthHandler()
+	handlerWithMiddleware := Auth(handler, authParams)
+
+	router := http.NewServeMux()
+	router.Handle("/hello", handlerWithMiddleware)
+
+	req := httptest.NewRequest("GET", "/hello", nil)
+	req.Header.Set("Authorization", headerValue)
+	w := httptest.NewRecorder()
+
+	router.ServeHTTP(w, req)
+
+	if w.Code != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v", w.Code, http.StatusOK)
+	}
+
+	if fakeLogger.ErrorMessage != "" {
+		t.Errorf("authParams.Logger.ErrorMessage is not empty")
+	}
+
+	if fakeLogger.InfoMessage != "User authenticated" {
+		t.Errorf("authParams.Logger.InfoMessage is not 'User authenticated'")
+	}
 }
 
 func TestMissingHeader(t *testing.T) {
