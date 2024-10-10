4d52ba96fbe1732c7c6f5559d90e96b069ea5340
Marius Fagerland <marius.fagerland@sparebank1.no>
2024-10-10 10:02:12 +0200

    Make routes exportable

--- middleware/routes.go
+++ middleware/routes.go
@@ -6,14 +6,14 @@ import (
 	"github.com/mariusfa/golf/logging/accesslog"
 )
 
-func privateRoute(endpoint http.Handler, authParams AuthParams) http.Handler {
+func PrivateRoute(endpoint http.Handler, authParams AuthParams) http.Handler {
 	middlewareWrapper := Auth(endpoint, authParams)
 	middlewareWrapper = RequestIdMiddleware(middlewareWrapper)
 	middlewareWrapper = AccessLogMiddleware(middlewareWrapper, accesslog.GetLogger())
 	return middlewareWrapper
 }
 
-func publicRoute(endpoint http.Handler) http.Handler {
+func PublicRoute(endpoint http.Handler) http.Handler {
 	middlewareWrapper := RequestIdMiddleware(endpoint)
 	middlewareWrapper = AccessLogMiddleware(middlewareWrapper, accesslog.GetLogger())
 	return middlewareWrapper
