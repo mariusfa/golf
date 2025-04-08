e3d83c5c92824de8281d35763ff35ff7f60432b4
Andreas Foldvik Kemkers <andreas.foldvik.kemkers@sparebank1.no>
2025-04-08 09:40:08 +0200

    fix

--- request/requestid-context.go
+++ request/requestid-context.go
@@ -15,14 +15,3 @@ func NewRequestIdCtx(requestId string) *RequestIdCtx {
 func WithRequestIdCtx(ctx context.Context, requestId string) context.Context {
 	return context.WithValue(ctx, RequestIdCtxKey, NewRequestIdCtx(requestId))
 }
-
-/*func GetRequestIdCtx(ctx context.Context) *RequestIdCtx {
-	if ctx == nil {
-		return nil
-	}
-	if requestIdCtx, ok := ctx.Value(RequestIdCtxKey).(*RequestIdCtx); ok {
-		return requestIdCtx
-	}
-	return nil
-}
-*/
