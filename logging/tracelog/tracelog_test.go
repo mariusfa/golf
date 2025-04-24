82dd5e2b455bb8a212829fdf6647d5282ce31173
Andreas Foldvik Kemkers <andreas.foldvik.kemkers@sparebank1.no>
2025-04-24 08:49:12 +0200

    legger error inn i payload isteden for å ha et separat felt

--- logging/tracelog/tracelog_test.go
+++ logging/tracelog/tracelog_test.go
@@ -3,38 +3,94 @@ package tracelog
 import (
 	"bytes"
 	"context"
-	"github.com/mariusfa/golf/request"
+	"errors"
 	"os"
 	"strings"
 	"testing"
+
+	"github.com/mariusfa/golf/request"
 )
 
-func TestTraceLog_Context(t *testing.T) {
+func TestTraceLog_Info(t *testing.T) {
 	ctx := setupContext()
 	output := captureStdout(func() {
 		SetAppName("testapp")
 		Info(ctx, "test payload")
 	})
 
-	expected := "\"log_type\":\"TRACE\""
+	expected := "\"log_level\":\"INFO\""
 	if !strings.Contains(output, expected) {
-		t.Errorf("Expected output to contain %q, but was not found. Ouput: %q", expected, output)
+		t.Errorf("Expected output to contain %q, but was not found. Output: %q", expected, output)
+	}
+	expected = "\"log_type\":\"TRACE\""
+	if !strings.Contains(output, expected) {
+		t.Errorf("Expected output to contain %q, but was not found. Output: %q", expected, output)
 	}
 	expected = "\"app_name\":\"testapp\""
 	if !strings.Contains(output, expected) {
-		t.Errorf("Expected output to contain %q, but was not found. Ouput: %q", expected, output)
+		t.Errorf("Expected output to contain %q, but was not found. Output: %q", expected, output)
 	}
 	expected = "\"payload\":\"test payload\""
 	if !strings.Contains(output, expected) {
-		t.Errorf("Expected output to contain %q, but was not found. Ouput: %q", expected, output)
+		t.Errorf("Expected output to contain %q, but was not found. Output: %q", expected, output)
 	}
 	expected = "\"request_id\":\"123\""
 	if !strings.Contains(output, expected) {
-		t.Errorf("Expected output to contain %q, but was not found. Ouput: %q", expected, output)
+		t.Errorf("Expected output to contain %q, but was not found. Output: %q", expected, output)
 	}
 	expected = "\"user_id\":\"test_username\""
 	if !strings.Contains(output, expected) {
-		t.Errorf("Expected output to contain %q, but was not found. Ouput: %q", expected, output)
+		t.Errorf("Expected output to contain %q, but was not found. Output: %q", expected, output)
+	}
+}
+
+func TestTraceLog_Error(t *testing.T) {
+	ctx := setupContext()
+	output := captureStdout(func() {
+		SetAppName("testapp")
+		Error(ctx, "test error payload")
+	})
+
+	expected := "\"log_level\":\"ERROR\""
+	if !strings.Contains(output, expected) {
+		t.Errorf("Expected output to contain %q, but was not found. Output: %q", expected, output)
+	}
+	expected = "\"log_type\":\"TRACE\""
+	if !strings.Contains(output, expected) {
+		t.Errorf("Expected output to contain %q, but was not found. Output: %q", expected, output)
+	}
+	expected = "\"app_name\":\"testapp\""
+	if !strings.Contains(output, expected) {
+		t.Errorf("Expected output to contain %q, but was not found. Output: %q", expected, output)
+	}
+	expected = "\"payload\":\"test error payload\""
+	if !strings.Contains(output, expected) {
+		t.Errorf("Expected output to contain %q, but was not found. Output: %q", expected, output)
+	}
+	expected = "\"request_id\":\"123\""
+	if !strings.Contains(output, expected) {
+		t.Errorf("Expected output to contain %q, but was not found. Output: %q", expected, output)
+	}
+	expected = "\"user_id\":\"test_username\""
+	if !strings.Contains(output, expected) {
+		t.Errorf("Expected output to contain %q, but was not found. Output: %q", expected, output)
+	}
+}
+
+func TestTraceLog_Errorf(t *testing.T) {
+	ctx := setupContext()
+	output := captureStdout(func() {
+		SetAppName("testapp")
+		Errorf(ctx, "test error payload", errors.New("error happened"))
+	})
+
+	expected := "\"log_level\":\"ERROR\""
+	if !strings.Contains(output, expected) {
+		t.Errorf("Expected output to contain %q, but was not found. Output: %q", expected, output)
+	}
+	expected = "\"payload\":\"test error payload: error happened\""
+	if !strings.Contains(output, expected) {
+		t.Errorf("Expected output to contain %q, but was not found. Output: %q", expected, output)
 	}
 }
 
@@ -46,11 +102,11 @@ func TestTraceLog_MissingContext(t *testing.T) {
 
 	expected := "\"request_id\":\"\""
 	if !strings.Contains(output, expected) {
-		t.Errorf("Expected output to contain %q, but was not found. Ouput: %q", expected, output)
+		t.Errorf("Expected output to contain %q, but was not found. Output: %q", expected, output)
 	}
 	expected = "\"user_id\":\"\""
 	if !strings.Contains(output, expected) {
-		t.Errorf("Expected output to contain %q, but was not found. Ouput: %q", expected, output)
+		t.Errorf("Expected output to contain %q, but was not found. Output: %q", expected, output)
 	}
 }
 
